3d9dc1fe72615a6c95a864c8e68fd876e10f161e
abdrrahmanboulvali <abdobolvalli145@gmail.com>
2026-02-02 15:58:37 +0000

    Add production settings for PythonAnywhere2

--- Systeme_gestion_transport/settings_prod.py
+++ Systeme_gestion_transport/settings_prod.py
@@ -8,6 +8,13 @@ CSRF_TRUSTED_ORIGINS = ["https://abderahman.pythonanywhere.com"]
 
 SECRET_KEY = os.environ.get("SECRET_KEY", SECRET_KEY)
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    }
+}
+
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
